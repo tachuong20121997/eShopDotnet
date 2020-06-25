f31f917d25e8adf6fcc3f14d96f9757a2273682f
chuongninolxag2 <chuongninolxag@gmail.com>
2020-06-25 21:57:45 +0700

    add Model

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Learn ASP.NET CORE 3.1 
 # Data Driven Design
+## Install Package
+	- Microsoft.EntityFrameworkCore.SqlServer
+	- Microsoft.EntityFrameworkCore.Design
+	- Microsoft.EntityFrameworkCore.Tools
---
+++ eShopSolution.Data/Entities/Shop/Caterogy.cs
@@ -0,0 +1,17 @@
+﻿using eShopSolution.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities.Shop
+{
+    public class Caterogy
+    {
+        public int Id { get; set; }
+        public decimal SortOrder { get; set; }
+        public decimal IsShowOnHome { get; set; }
+        public int? ParentId { get; set; }
+        public Status Status { get; set; }
+     
+    }
+}
